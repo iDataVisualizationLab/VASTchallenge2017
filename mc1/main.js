1c77ed7a05fe506461054460b8f7110e95ba5608
Long Nguyen <litpuvn@gmail.com>
2017-05-20 17:20:23 -0500

    add initial coding

---
+++ mc1/main.js
@@ -0,0 +1,8 @@
+var MAP_WIDTH = 200;
+var MAP_HEIGHT = 200;
+var mapFile = 'data/Lekagul Roadways.bmp';
+
+let mapData = createMapByteData(MAP_WIDTH, MAP_HEIGHT, mapFile);
+
+debugger;
+
---
+++ mc1/src/util.js
@@ -0,0 +1,17 @@
+/**
+ * Create byte array of a map, each pixel represents by 4 consecutive elements that stand for Red, Green, Blue and Alpha
+ * @param width
+ * @param height
+ * @param referenceBitmapImagePath path to reference bitmap
+ */
+function createMapByteData(width, height, referenceBitmapImagePath) {
+    var canvas =  document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+
+    var ctx = canvas.getContext('2d');
+    var myImg = new Image();
+    myImg.src = referenceBitmapImagePath;
+    ctx.drawImage(myImg, 0, 0);
+    return ctx.getImageData(0, 0, MAP_WIDTH, MAP_HEIGHT).data;
+}
