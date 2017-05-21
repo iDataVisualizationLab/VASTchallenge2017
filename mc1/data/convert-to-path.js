3508802d3348c3a3499ce3d5649c0e2c27f165e5
Long Nguyen <litpuvn@gmail.com>
2017-05-21 15:50:32 -0500

    add data conversion tool

---
+++ mc1/data/convert-to-path.js
@@ -0,0 +1,96 @@
+var fs = require('fs');
+var parse = require('csv-parse');
+var jsonfile = require('jsonfile');
+
+
+
+var file = 'Lekagul Sensor Data.csv';
+var mappingUniprotToMgi = {};
+
+var readExistingSensorData = function() {
+    let myCarData = {};
+    let myCar = {};
+
+    let ignoreHeader = true;
+    let tmpCar;
+
+    let tmpGateName;
+    let tmpGateTime;
+    let time;
+    let carId;
+    let outliner = {};
+    let cars;
+
+    fs.createReadStream(file)
+        .pipe(parse({delimiter: ','}))
+        .on('data', function(csvrow, i) {
+            if (ignoreHeader) {
+                ignoreHeader = false;
+                return;
+            }
+
+            time = csvrow[0];
+            carId = csvrow[1];
+            tmpGateName = csvrow[3];
+            // gate timestamp record
+            tmpGateTime = {};
+            tmpGateTime[tmpGateName] = time;
+
+            if (!myCar.hasOwnProperty(carId)) {
+                tmpCar = new Object();
+                tmpCar.carId = carId;
+                tmpCar.carType = csvrow[2];
+                tmpCar.path = [];
+                tmpCar.enter = false;
+                tmpCar.exit = false;
+
+                myCar[carId] = tmpCar;
+            }
+
+            tmpCar = myCar[carId];
+            if (tmpCar.path.length < 1 && (tmpCar.carType != '2P' && !tmpGateName.startsWith('entrance') || tmpCar.carType == '2P' && !tmpGateName.startsWith('ranger-base') )) {
+                console.error('This car has not entered the park. carId=' + tmpCar.carId + "; time=" + tmpCar.time);
+                outliner[tmpCar.carId] = tmpCar;
+            }
+
+            // ignore outliner
+            if (outliner.hasOwnProperty(carId)) {
+                console.error('This car is in outlier list. id=' + carId);
+                return;
+            }
+
+            // add current hop to path
+            tmpCar.path.push(tmpGateTime);
+            if (tmpCar.carType != '2P' && tmpGateName.startsWith('entrance') || tmpCar.carType == '2P' && tmpGateName.startsWith('ranger-base') ) {
+                if (tmpCar.exit) {
+                    throw new Error('Car has already exit. Invalid data processing for car id: ' + carId);
+                }
+
+                if (tmpCar.enter) {
+                    tmpCar.exit = true;
+                }
+                else {
+                    tmpCar.enter = true;
+                }
+            }
+
+            if (!myCarData.hasOwnProperty(tmpCar.carType)) {
+                myCarData[tmpCar.carType] = [];
+            }
+
+            cars = myCarData[tmpCar.carType];
+            if (tmpCar.exit) {
+                cars.push(myCar[carId]);
+                delete myCar[carId];
+            }
+
+        })
+        .on('end',function() {
+            jsonfile.writeFile("car-path.json", myCarData, function (err) {
+                console.error(err)
+            });
+        });
+};
+
+readExistingSensorData();
+
