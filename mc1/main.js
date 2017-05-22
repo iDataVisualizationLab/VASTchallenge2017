ba05683dd1304f1ab9c8ff0dedda2c8e33049138
Long Nguyen <litpuvn@gmail.com>
2017-05-22 16:38:32 -0500

    adding visit duration handling class

--- mc1/main.js
+++ mc1/main.js
@@ -31,35 +31,9 @@ var visitDurationSvg = d3.select('body').select('#visitDuration').append('svg')
     ;
 
 d3.json("data/all-car-path.json", function(error, lines) {
-// d3.csv("data/data-test.csv", function(error, paths) {
+    let visitChart = new Chart2D(visitDurationSvg, width, height, {margin: margin, timeChart: true});
 
-    // parse the date / time
-    let parseTime = d3.timeParse("%Y-%m-%d %H:%M:%S");
-
-
-    let visitDuration = new Chart2D(visitDurationSvg, width, height, {margin: margin, timeChart: true});
-    let minDate = parseTime('2015-05-01 00:43:28');
-    let maxDate = parseTime('2016-05-31 23:56:06');
-
-    visitDuration.setXDomain(minDate, maxDate);
-    visitDuration.setYDomain(0, 20000);
-    var colorFunction = d3.scaleOrdinal(d3.schemeCategory10);
-
-    lines.forEach(function(line, index) {
-
-        let colorIdx = line.carType;
-        let color = line.carType == '2P' ? '#000000' : colorFunction(colorIdx);
-        line.path.forEach(function (timeGate) {
-            // debugger;
-            timeGate.time = parseTime(timeGate.time);
-            timeGate.y = 50 + index;
-        });
-
-        visitDuration.addData(line.path, 'time', 'y', color);
-
-    });
-
-    visitDuration.renderChart();
-    visitDuration.renderAxis('Time', 'Visit');
+    let visitDuration = new VisitDuration(visitChart);
+    visitDuration.render(lines);
 
 });
--- mc1/src/visit-duration.js
+++ mc1/src/visit-duration.js
@@ -1,66 +1,40 @@
-var VisitDuration = function VisitDuration(svg, width, height) {
-    this.svg = svg;
-    this.width = width;
-    this.height = height;
+var VisitDuration = function VisitDuration(visitChart) {
+
+    this.visitChart = visitChart;
+    this.parseTime = d3.timeParse("%Y-%m-%d %H:%M:%S");
+
+    let minDate = this.parseTime('2015-05-01 00:43:28');
+    let maxDate = this.parseTime('2016-05-31 23:56:06');
+
+    this.visitChart.setXDomain(minDate, maxDate);
+    this.visitChart.setYDomain(0, 20000);
 };
 
-VisitDuration.prototype.render = function render(data) {
 
-    let svg = this.svg;
-    let width = this.width;
-    let height = this.height;
-// parse the date / time
-    var parseTime = d3.timeParse("%d-%b-%y");
-
-// set the ranges
-    var x = d3.scaleTime().range([0, width]);
-    var y = d3.scaleLinear().range([height, 0]);
-
-// define the line
-    var valueline = d3.line()
-        .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.close); });
+VisitDuration.prototype.init = function init(data) {
 
 
-    // format the data
-    data.forEach(function(d) {
-        d.date = parseTime(d.date);
-        d.close = +d.close;
+};
+VisitDuration.prototype.render = function render(lines) {
+
+    let colorFunction = d3.scaleOrdinal(d3.schemeCategory10);
+    // parse the date / time
+    let parseTime = d3.timeParse("%Y-%m-%d %H:%M:%S");
+    let self = this;
+    lines.forEach(function(line, index) {
+
+        let colorIdx = line.carType;
+        let color = line.carType == '2P' ? '#000000' : colorFunction(colorIdx);
+        line.path.forEach(function (timeGate) {
+            // debugger;
+            timeGate.time = self.parseTime(timeGate.time);
+            timeGate.y = 50 + index;
+        });
+
+        self.visitChart.addData(line.path, 'time', 'y', color);
+
     });
 
-    // Scale the range of the data
-    x.domain(d3.extent(data, function(d) { return d.date; }));
-    y.domain([0, d3.max(data, function(d) { return d.close; })]);
-
-    // Add the valueline path.
-    svg.append("path")
-        .data([data])
-        .attr("class", "line")
-        .attr("d", valueline);
-
-    // Add the x Axis
-    svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
-    // text label for the x axis
-    svg.append("text")
-        .attr("transform",
-            "translate(" + (width/2) + " ," +
-            (height + margin.top + 20) + ")")
-        .style("text-anchor", "middle")
-        .text("Date");
-
-    // Add the y Axis
-    svg.append("g")
-        .call(d3.axisLeft(y));
-
-    // text label for the y axis
-    svg.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin.left)
-        .attr("x",0 - (height / 2))
-        .attr("dy", "1em")
-        .style("text-anchor", "middle")
-        .text("Value");
+    this.visitChart.renderChart();
+    this.visitChart.renderAxis('Time', 'Visit');
 };
