1a456468cff4a00ba5a862d50e59bb3095f0a3c1
Md. Yasin Kabir <mykabir0@gmail.com>
2017-05-25 13:11:21 -0500

    updates in MC2

--- mc2/main.js
+++ mc2/main.js
@@ -1,5 +1,5 @@
 var svg = d3.select("svg"),
-    margin = {top: 20, right: 80, bottom: 30, left: 50},
+    margin = {top: 20, right: 150, bottom: 30, left: 50},
     width = svg.attr("width") - margin.left - margin.right,
     height = svg.attr("height") - margin.top - margin.bottom,
     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -20,8 +20,15 @@ var line = d3.line()
         else
         return y(d.Reading);
      });
+var chems = ["Appluimonia","Chlorodinine","Methylosmolene","AGOC-3A"];
 
-var varChems = ["Appluimonia","Chlorodinine","Methylosmolene","AGOC-3A"];
+var varChems = [];
+for(i=1;i<10;i++){
+  for(j=0;j<4;j++){
+    var id = chems[j]+ "_sensor" + i;
+    varChems.push(id);
+  }
+}
 var minDate = parseTime("4/1/16 00:00");
 var maxDate = parseTime("3/31/17 00:00");
 
@@ -36,12 +43,14 @@ d3.csv("data/sensorData.csv", function(error, data) {
   console.log(data);
 
   var chemicals = varChems.map(function(id) {
-
     return {
       id: id,
       values: data.map(function(d) {
          // && d.date.getMonth() == 3
-        if(d.Chemical == id && d.Monitor == "1" )
+         var n = id.indexOf("_");
+         var l = id.length;
+         // console.log(id.substring(l-1, l))
+        if(d.Chemical == id.substring(0, n) && d.Monitor == id.substring(l-1, l))
             return {date: d.date, Reading: d.Reading};
         else
             return 0;
@@ -59,7 +68,7 @@ console.log(chemicals)
     });
     chemicalsData.push(obj);
 }
-console.log(minDate)
+console.log(chemicalsData)
 console.log([minDate, maxDate])
   // x.domain(d3.extent(data, function(d) { return d.date; }));
   x.domain([minDate, maxDate]);
@@ -91,7 +100,11 @@ var tickLabels = ["April","","","","August","","","","December"];
   var chemical = g.selectAll(".chemical")
     .data(chemicalsData)
     .enter().append("g")
-      .attr("class", "chemical");
+      .attr("class", "chemical")
+      .attr("id", function(d,i){
+        // console.log(d);
+        return d.id;
+      })
 
 
 
@@ -101,8 +114,8 @@ console.log(parseTime("8/01/16 01:00") +" here "+ x(parseTime("8/1/16 01:00")));
     chemical.append("path")
       .attr("class", "line")
       .attr("d", function(d) { return line(d.values); })
-      .style("stroke", function(d) { return z(d.id); })
-      .style("stroke-width", .5);
+      .style("stroke", function(d,i) { return z(i); })
+      .style("stroke-width", 1);
 
   chemical.append("text")
       .datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
@@ -111,7 +124,10 @@ console.log(parseTime("8/01/16 01:00") +" here "+ x(parseTime("8/1/16 01:00")));
       .attr("dy", "0.35em")
       .style("font", "10px sans-serif")
       .text(function(d) { return d.id; })
-      .style("fill", function(d) { return z(d.id); });
+      .style("fill", function(d,i) { return z(i); });
+
+      d3.selectAll(".sensor_selection").on("change",sensorData);
+      d3.selectAll(".chemical_selection").on("change",sensorData);
 });
 
 function xx(date2){
@@ -131,5 +147,32 @@ function xx(date2){
 
     return x(date2);
 }
+function sensorData(d){  
+  var id = this.id.substring(1,2);
+  for(i=0;i<36;i++){
+    var l = varChems[i].length;
+    var sensorId = varChems[i].substring(l-1,l);
+    var n = varChems[i].indexOf("_");
+    // console.log(varChems[i].substring(0,n))
+      if(document.getElementById("S"+sensorId).checked && document.getElementById(varChems[i].substring(0,n)).checked){
+        // console.log(document.getElementById("S"+sensorId) + " " + varChems[i].substring(0,n))
+        document.getElementById(varChems[i]).style.display = "";
+      }
+      else
+        document.getElementById(varChems[i]).style.display = "none";
 
+  }
+}
 
+$(document).ready(function() {
+  $('#checkall').click(function() {
+    var checked = $(this).prop('checked');
+    $('#sensorOptions').find('input:checkbox').prop('checked', checked);
+  });
+})
+$(document).ready(function() {
+  $('#checkallChems').click(function() {
+    var checked = $(this).prop('checked');
+    $('#chemicalOptions').find('input:checkbox').prop('checked', checked);
+  });
+})
